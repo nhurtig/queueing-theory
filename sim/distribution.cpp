a75c8768a337ed849e9c1869ad71db5fca26a7ba
nathurtig@gauss <nhurtig@acm.org>
2023-09-24 10:53:34 -0400

    discrete distribution

--- sim/distribution.cpp
+++ sim/distribution.cpp
@@ -1,6 +1,9 @@
 #include "distribution.h"
 #include <random>
 #include <chrono>
+#include <numeric>
+#include <functional>
+#include <stdexcept>
 
 // from https://stackoverflow.com/questions/9878965/rand-between-0-and-1
 uint64_t timeSeed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
@@ -21,9 +24,9 @@ ExponentialDistribution::ExponentialDistribution(real lambda) {
     this->lambda = lambda;
 }
 
-real ExponentialDistribution::pdf(real x) {
-    return this->lambda*exp(-this->lambda*x);
-}
+// real ExponentialDistribution::pdf(real x) {
+    // return this->lambda*exp(-this->lambda*x);
+// }
 
 real ExponentialDistribution::cdf(real x) {
     return 1-exp(-this->lambda*x);
@@ -31,4 +34,49 @@ real ExponentialDistribution::cdf(real x) {
 
 real ExponentialDistribution::sample() {
     return -ln(-rand_real()+1)/this->lambda;
+}
+
+DiscreteDistribution::DiscreteDistribution(std::vector<real> vals, std::vector<real> probs) {
+    real sum = std::accumulate(probs.begin(), probs.end(), 0.0);
+    std::for_each(probs.begin(), probs.end(), [sum](real x){return x/sum;});
+
+    this->size = vals.size();
+    if (this->size != probs.size()) {
+        throw std::invalid_argument("Value and probability vectors should be same length!");
+    }
+
+    real lastx = 0;
+    for (const auto& x : vals) {
+        if (lastx >= x) {
+            throw std::invalid_argument("Discrete values should be positive and increasing!");
+        }
+        lastx = x;
+    }
+
+    this->vals = vals;
+    this->probs = probs;
+}
+
+real DiscreteDistribution::cdf(real x) {
+    real result = 0;
+
+    for (unsigned int i = 0; i < this->size; i++) {
+        if (x < vals[i]) {
+            result += probs[i];
+        }
+    }
+
+    return result;
+}
+
+real DiscreteDistribution::sample() {
+    real x = rand_real();
+    for (unsigned int i = 0; i < this->size; i++) {
+        if (x < probs[i]) {
+            return vals[i];
+        }
+        x -= probs[i];
+    }
+
+    throw std::out_of_range("Did probs not add to 1?");
 }
--- sim/distribution.h
+++ sim/distribution.h
@@ -1,6 +1,7 @@
 #ifndef DISTRIBUTION_H
 #define DISTRIBUTION_H
 #include "queue_standard.h"
+#include <vector>
 
 real rand_real();
 
@@ -8,7 +9,7 @@ void seed_rand(int);
 
 class Distribution {
 public:
-    virtual real pdf(real) = 0;
+    // virtual real pdf(real) = 0;
     virtual real cdf(real) = 0;
     virtual real sample() = 0;
 };
@@ -16,11 +17,22 @@ public:
 class ExponentialDistribution: public Distribution {
     public:
         ExponentialDistribution(real);
-        real pdf(real);
+        // real pdf(real);
         real cdf(real);
         real sample();
     private:
         real lambda;
+};
+
+class DiscreteDistribution: public Distribution {
+    public:
+        DiscreteDistribution(std::vector<real> vals, std::vector<real> probs);
+        real cdf(real);
+        real sample();
+    private:
+        unsigned int size;
+        std::vector<real> vals;
+        std::vector<real> probs;
 };
 
 #endif
