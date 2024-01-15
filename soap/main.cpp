dd873bc68838c208a6cfbbc4d155030428a9fcca
bastion <hurtigna@rose-hulman.edu>
2024-01-15 10:54:13 -0500

    now works?

--- soap/main.cpp
+++ soap/main.cpp
@@ -13,11 +13,11 @@ int main(int argc, char **argv) {
 /*
     ExponentialDistribution dist = ExponentialDistribution(4.0);
 
-    Job job = Job(&dist, 7.3, 3.9);
+    Job job = Job(7.4, &dist);
 
     DataStore store;
 
-    store.addJob(job, 34);
+    store.addJob(&job, 34);
 
     printf("Store size: %ld\n", store.getJobs().size());
     store.dumpJobs();
@@ -36,7 +36,15 @@ int main(int argc, char **argv) {
     printf("Stream til interrupt: %Lf\n", stream.nextInterrupt());
     printf("Stream's job pop til interrupt: %Lf\n", stream.popJob(3).nextInterrupt());
     printf("Stream til interrupt: %Lf\n", stream.nextInterrupt());
+
+    FCFSPolicy pol;
+    PolicyManager mgr(3, &pol);
+
+    mgr.addJob(job);
+    mgr.nextInterrupt();
+    mgr.serve(3);
 */
+
 
     unsigned int k = 5;
     FCFSPolicy policy;
